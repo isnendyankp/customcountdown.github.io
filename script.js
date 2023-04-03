61bbcada7e3b39ac8dd7a6098d505ec3e1c2ef9d
isnendyan <isnendyankp@gmail.com>
2023-04-03 13:05:46 +0700

    add date input for request

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const dateEl = document.getElementById('date-picker');
 
 // Set Date Input Min & Value with Today's Date
 const today = new Date().toISOString().split('T')[0];
-console.log(today);
+dateEl.setAttribute('min', today);
