bb3a2d2a819fa8d4a8fe2a7759ad8f87643f3cd5
isnendyan <isnendyankp@gmail.com>
2023-04-03 13:01:40 +0700

    create set Date today with console.log

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+// Set Date Input Min & Value with Today's Date
+const today = new Date().toISOString();
+console.log(today);
