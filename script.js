082ee480b13fd7f0c9ab57b9a1ed1ea5d4579b28
isnendyan <isnendyankp@gmail.com>
2023-04-04 09:58:55 +0700

    Create Event Listner & take values from form input

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const dateEl = document.getElementById('date-picker');
 // Set Date Input Min & Value with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take values from Form Input
+function updateCountdown(e) {
+    console.log(e);
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
