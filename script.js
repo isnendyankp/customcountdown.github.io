b09e97195e36ecf42f02aa326b4582b0ca1c66fd
isnendyan <isnendyankp@gmail.com>
2023-04-04 10:09:59 +0700

    countdownTitle & countdownDate being passed

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min & Value with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 
 // Take values from Form Input
 function updateCountdown(e) {
-    e.prevenDefault();
-    console.log(e);
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
 }
 
 // Event Listener
