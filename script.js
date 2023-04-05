21192e588222687bb81c1946f6256c49cbd2fce8
isnendyan <isnendyankp@gmail.com>
2023-04-05 08:16:31 +0700

    get distance at  countdown value

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
@@ -9,6 +14,13 @@ let countdownValue = Date;
 // Set Date Input Min & Value with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Populate Countdown / Complete UI
+function updateDOM() {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+    console.log('distance', distance);
+  }
 
 // Take values from Form Input
 function updateCountdown(e) {
@@ -19,6 +31,7 @@ function updateCountdown(e) {
     // Get number version of current Date, updateDOM
     countdownValue = new Date(countdownDate).getTime();
     console.log('countdown value:', countdownValue);
+    updateDOM();
 }
 
 // Event Listener
